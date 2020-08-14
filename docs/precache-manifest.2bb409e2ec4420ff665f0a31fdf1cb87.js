a0f7e1340fae72eeca4f3c82d932ff0aeaead682
sergio <karmic1090@gmail.com>
2020-08-13 23:03:17 -0500

    Agregando docs

---
+++ docs/precache-manifest.2bb409e2ec4420ff665f0a31fdf1cb87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cae71602e2ea965f3494f3d7bf0039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe7f5a785d9cd07f6a80",
+    "url": "/static/css/main.53c16e81.chunk.css"
+  },
+  {
+    "revision": "0ec701a8c338deb31373",
+    "url": "/static/js/2.fdb73699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fdb73699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7f5a785d9cd07f6a80",
+    "url": "/static/js/main.a0a07070.chunk.js"
+  },
+  {
+    "revision": "4118261bad4d6485cd59",
+    "url": "/static/js/runtime-main.93e9af04.js"
+  }
+]);
